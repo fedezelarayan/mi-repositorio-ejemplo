f5560422faf469ad56781b14d0991c54b63056e8
fedezelarayan <federico.zelarayan@hotmail.com>
2026-05-05 13:14:16 -0300

    sprint 2 tp 2

---
+++ app.js
@@ -0,0 +1,62 @@
+import dns from "node:dns";
+import mongoose from "mongoose";
+import { type } from "node:os";
+
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
+
+mongoose.connect('mongodb+srv://grupo-28:grupo-28@cluster0.blryo.mongodb.net/NodeMod3Cohorte5')
+.then(() => console.log('Conexión exitosa a MongoDB'))
+.catch(error => console.error('Error al conectar a MongoDB:', error));
+
+const superheroSchema = new mongoose.Schema({
+    nombreSuperHeroe: { type: String, required: true },
+    nombreReal: { type: String, required: true },
+    edad: { type: Number, min: 0 },
+    planetaOrigen: { type: String, default: "Unknown" },
+    debilidad: String,
+    poderes: [String],
+    aliados: [String],
+    enemigos: [String],
+    createdAt: { type: Date, default: Date.now },
+    creador: String
+}, { collection: 'Grupo-28' });
+
+const SuperHero = mongoose.model('SuperHero', superheroSchema);
+
+async function insertSuperHero() {
+    const hero = new SuperHero({
+        nombreSuperHeroe: "Spiderman",
+        nombreReal: "Peter Parker",
+        edad: 25,
+        planetaOrigen: "Tierra",
+        debilidad: "Radioactiva",
+        poderes: ["Agilidad", "Super fuerza", "Sentido arácnido", "Trepar paredes"],
+        aliados: ["Ironman"],
+        enemigos: ["Duende Verde"],
+        creador: "Martin"
+    })
+    await hero.save();
+    console.log('Superhéroe insertado:', hero);
+}
+insertSuperHero();
+
+async function updateSuperHero(nombreSuperHeroe) {
+    const result = await SuperHero.updateOne(
+        { nombreSuperHeroe: nombreSuperHeroe },
+        { $set: { edad: 26 } }
+    );
+    console.log('Superhéroe actualizado:', result);
+}
+updateSuperHero("Spiderman");
+
+async function deleteSuperHero(nombreSuperHeroe) {
+    const result = await SuperHero.deleteOne({ nombreSuperHeroe: nombreSuperHeroe });
+    console.log('Superhéroe eliminado:', result);
+}
+deleteSuperHero("Spiderman");
+
+async function findSuperHeroes() {
+    const heroes = await SuperHero.find({ planetaOrigen: "Tierra" });
+    console.log('Superhéroes encontrados:', heroes);
+}
+findSuperHeroes();
